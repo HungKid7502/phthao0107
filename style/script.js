94e0b5cc3301f2c43dcbddd54da88c2948ebf470
Kid-hl <hungcypher7502@gmail.com>
2026-09-25 11:09:09 +0700

    phthao0107

--- style/script.js
+++ style/script.js
@@ -13,19 +13,19 @@ const lanternImages = [];
 for (let i = 1; i <= 9; i++) lanternImages.push(`./style/img/lantern/ld (${i}).png`);
 
 const messages = [
-    { text: "Chúc chị bé của em Trung Thu vui vẻ, ấm áp và luôn khỏe mạnh, bình an! 😊", img: "https://i.pinimg.com/originals/fc/cb/07/fccb0736aba4e6892cf1dabe9f6866d6.gif" },
-    { text: "Trung Thu này hong cần quà, chỉ cần chị thoi! 🎁", img: "https://i.pinimg.com/originals/75/35/89/753589ee385233757d931b8800cc7475.gif" },
-    { text: "Trung thu này nếu chị chưa có người thương, hong xao hong xao, có e thương^^", img: "https://i.pinimg.com/originals/60/b4/f9/60b4f9c20f6b6536bc232355030c4b7a.gif" },
-    { text: "Người ta ngắm trăng, còn em lại ngắm chị — vì chị rực rỡ hơn cả đêm Trung Thu. 💖🌙", img: "./style/img/Anh (4).jpg" },
-    { text: "Nếu có điều ước trong đêm trăng rằm, em chỉ muốn được nhìn thấy chị ngoài đời thêm một lần", img: "./style/img/Anh (1).jpg" }, 
-    { text: "Nếu có thể gửi lời chúc theo ánh trăng, em muốn nhắn rằng: ‘Chị ơi, mãi tỏa sáng nha! 🌟 ", img: "./style/img/Anh (11).jpg" }, 
-    { text: "Trung Thu này, có chị là đủ ngọt ngào hơn mọi loại bánh 🍰", img: "./style/img/Anh (3).jpg" }, 
-    { text: "Bánh Trung Thu có nhân gì cũng được, miễn là trong tim em luôn có ‘nhân vật chính’ là chị 🥮", img: "./style/img/Anh (9).jpg" }, 
-    { text: "Trăng tròn rồi sẽ khuyết, nhưng tình cảm của fan nhỏ này dành cho chị thì không bao giờ phai 🌙", img: "https://i.pinimg.com/originals/8e/d5/3a/8ed53a260b997abc3dc6bef98a71192b.gif" }, 
-    { text: "Đêm nay trăng tròn, tim em cũng tròn — vì chị đã chiếm trọn cả hai 🧡", img: "./style/img/Anh (5).jpg" }, 
-    { text: "Nếu mỗi ngọn đèn lồng là một lời chúc, thì em xin thắp cả ngàn đèn để chúc chị luôn hạnh phúc 🏮", img: "./style/img/Anh (7).jpg" }, 
-    { text: "Đêm Trung Thu gió mát, trăng trong, nhưng không gì làm em rung động bằng giọng nói của chị 🍂 ", img: "./style/img/Anh (2).jpg" },
-    { text: "Chị Hằng trong đêm trăng thì cũng đẹp đấy, nhưng lộng lẫy nhất là chị Hà cơ ✨ ", img: "./style/img/Anh (6).jpg" }
+    { text: "Chúc bé Thảo Trung Thu vui vẻ, ấm áp và luôn khỏe mạnh, bình an! 😊", img: "./style/img/Anh (5).gif" },
+    { text: "Trung Thu này hong cần quà, chỉ cần em thoi! 🎁", img: "https://i.pinimg.com/originals/75/35/89/753589ee385233757d931b8800cc7475.gif" },
+    { text: "Trung thu này nếu em chưa có người thương, hong xao hong xao, có anh thương^^", img: "https://i.pinimg.com/originals/60/b4/f9/60b4f9c20f6b6536bc232355030c4b7a.gif" },
+    { text: "Người ta ngắm trăng, còn anh lại ngắm em — vì em rực rỡ hơn cả đêm Trung Thu. 💖🌙", img: "./style/img/Anh (4).gif" },
+    { text: "Nếu có một điều ước trong đêm trăng rằm, anh chỉ muốn mỗi ngày đều được nhìn thấy em", img: "./style/img/Anh (1).jpg" }, 
+    { text: "Nếu có thể gửi lời chúc theo ánh trăng, anh sẽ nhắn rằng: ‘Thảo ơi, em mãi tỏa sáng nha! 🌟 ", img: "https://i.pinimg.com/originals/88/23/82/882382f97862c72e60fc06822e36eb55.gif" }, 
+    { text: "Trung Thu này, có em là đủ ngọt ngào hơn mọi loại bánh 🍰", img: "./style/img/Anh (3).gif" }, 
+    { text: "Bánh Trung Thu có nhân gì cũng được, miễn là trong tim anh luôn có ‘nhân vật chính’ là Thảo 🌿", img: "./style/img/thao.jpg" }, 
+    { text: "Trăng tròn rồi sẽ khuyết, nhưng tình cảm của anh này dành cho em thì không bao giờ phai 🌙", img: "https://i.pinimg.com/originals/8e/d5/3a/8ed53a260b997abc3dc6bef98a71192b.gif" }, 
+    { text: "Đêm nay trăng tròn, tim anh cũng tròn — vì Thảo đã chiếm trọn cả hai 🧡", img: "https://i.pinimg.com/originals/fc/cb/07/fccb0736aba4e6892cf1dabe9f6866d6.gif" }, 
+    { text: "Nếu mỗi ngọn đèn lồng là một lời chúc, thì anh xin thắp cả ngàn đèn để chúc Thảo luôn hạnh phúc 🏮", img: "./style/img/Anh (7).jpg" }, 
+    { text: "Đêm Trung Thu gió mát, trăng trong, nhưng không gì làm anh rung động bằng giọng nói của em 🍂 ", img: "./style/img/Anh (2).jpg" },
+    { text: "Chị Hằng trong đêm trăng thì cũng đẹp đấy, nhưng lộng lẫy nhất là Thảo cơ ✨ ", img: "./style/img/Anh (6).gif" }
 ];
 
 const lanternsContainer = document.getElementById("lanternsContainer");
